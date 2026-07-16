be946b751d1efc86ad3f2e61ba54b97483414ee8
Owen Meyers <omeyers31@gmail.com>
2026-07-15 22:42:30 -0500

    Refactor OrderQueue for concurrency via mutex and condition variable.

--- PharmaServe/OrderQueue.cpp
+++ PharmaServe/OrderQueue.cpp
@@ -1,11 +1,21 @@
 #include "OrderQueue.h"
 
 void OrderQueue::Push(const Order& order) {
+// Adds an order and wakes a consumer
+	{
+	std::lock_guard<std::mutex> lock(mutex);
 	orderQueue.push(order);
+	}
+	cv.notify_one();
 }
 
 bool OrderQueue::Pop(Order& outOrder) {
-// Pop the front order into `outOrder` parameter
+// Wait to be notified about an order in the queue.
+	std::unique_lock<std::mutex> lock(mutex);
+	cv.wait(lock, [this] { return !orderQueue.empty() || shuttingDown;});
+//	Only use this method within a worker thread.
+//	If called from the main thread, it will freeze if the queue is empty.
+
 	if (orderQueue.empty()) {
 		return false;
 	}
@@ -15,10 +25,12 @@ bool OrderQueue::Pop(Order& outOrder) {
 }
 
 size_t OrderQueue::Size() const {
+	std::lock_guard<std::mutex> lock(mutex);
 	return orderQueue.size();
 }
 
 bool OrderQueue::IsEmpty() const {
+	std::lock_guard<std::mutex> lock(mutex);
 	return orderQueue.empty();
 }
 
--- PharmaServe/OrderQueue.h
+++ PharmaServe/OrderQueue.h
@@ -1,17 +1,28 @@
 #pragma once
-#include "Order.h"
 #include <queue>
+#include <mutex>
+#include <condition_variable>
+
+#include "Order.h"
 
 class OrderQueue {
     private: 
+        mutable std::mutex mutex;
+        std:: condition_variable cv;
         std::queue<Order> orderQueue;
+        bool shuttingDown = false;
+
+
     public:
         void Push(const Order& order);
     
-        bool Pop(Order& order);
+        bool Pop(Order& outOrder);
+
+        bool TryPop(Order& outOrder);
         
 		size_t Size() const;
-
 		bool IsEmpty() const;
+
+        void Shutdown();
 
 };
