7c51428ec290589b48c62c6eaa52678d775a8c2c
Owen Meyers <omeyers31@gmail.com>
2026-07-15 23:15:29 -0500

    Test concurrency with simulation data in main.

--- PharmaServe/PharmaServe.cpp
+++ PharmaServe/PharmaServe.cpp
@@ -2,7 +2,18 @@
 #include "OrderQueue.h"
 
 
-
+void Worker(OrderQueue& orderQueue) {
+	Order order;
+	while (orderQueue.Pop(order)) {
+		std::cout << "[worker] dispensing order " << order.orderId 
+				  << " (" << order.drugName << " x" << order.quantity << ")\n";
+		// Wait for .4s to simulate the robot working.
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		std::cout << "[worker] completed order " << order.orderId << ".\n";
+		std::cout << "Pending Orders: " << orderQueue.Size() << std::endl;
+	}
+	std::cout << "[worker] shutting down...\n";
+}
 
 int main()
 {
@@ -10,19 +21,18 @@ int main()
 
     OrderQueue orderQueue;
 
+	std::thread worker(Worker, std::ref(orderQueue));
+
 	std::cout << "\n[Ingesting Orders from source...]" << std::endl;
 	orderQueue.Push({ 1, "Aspirin", 100 });
     orderQueue.Push({ 2, "Ibuprofen", 200 });
 	orderQueue.Push({ 3, "Tylenol", 150 });
+	std::this_thread::sleep_for(std::chrono::milliseconds(500));
+	orderQueue.Push({ 4, "Amoxicillin", 30});
 
-	std::cout << "Pending Orders: " << orderQueue.Size() << std::endl;
-	Order currentOrder;
-	while (orderQueue.Pop(currentOrder)) {
-		std::cout << " -> Dispensing Order #" << currentOrder.orderId
-			<< ": " << currentOrder.quantity << "x "
-			<< currentOrder.drugName << std::endl;
-	}
-	std::cout << "\nQueue processing complete. Remaining: " << orderQueue.Size() << std::endl;
+	std::this_thread::sleep_for(std::chrono::seconds(3));
+	orderQueue.Shutdown();
+	worker.join();
 
     return 0;
 }
