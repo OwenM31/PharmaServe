50dc3caa36da7b9a7178567629dab0362de20d29
Owen Meyers <omeyers31@gmail.com>
2026-07-16 21:59:35 -0500

    Add WaitUntilEmpty method to OrderQueue.

--- PharmaServe/OrderQueue.cpp
+++ PharmaServe/OrderQueue.cpp
@@ -1,57 +1,62 @@
 #include "OrderQueue.h"
+#include <mutex>
 
-void OrderQueue::Push(const Order& order) {
-// Adds an order and wakes a `WorkerPop` thread.
-	{
-	std::lock_guard<std::mutex> lock(mutex);
-	orderQueue.push(order);
-	}
-	cv.notify_one();
+void OrderQueue::Push(const Order &order) {
+  // Adds an order and wakes a `WorkerPop` thread.
+  {
+    std::lock_guard<std::mutex> lock(mutex);
+    orderQueue.push(order);
+  }
+  cv.notify_one();
 }
 
-bool OrderQueue::Pop(Order& outOrder) {
-// Safe to call from main thread.
-	std::lock_guard<std::mutex> lock(mutex);
-	if (orderQueue.empty()) {
-		return false;
-	}
-	outOrder = orderQueue.front();
-	orderQueue.pop();
-	return true;
+bool OrderQueue::Pop(Order &outOrder) {
+  // Safe to call from main thread.
+  std::lock_guard<std::mutex> lock(mutex);
+  if (orderQueue.empty()) {
+    return false;
+  }
+  outOrder = orderQueue.front();
+  orderQueue.pop();
+  return true;
 }
 
-bool OrderQueue::WorkerPop(Order& outOrder) {
-	// Wait to be notified about an order in the queue.
-	std::unique_lock<std::mutex> lock(mutex);
-	cv.wait(lock, [this] { return !orderQueue.empty() || shuttingDown;});
-	//	Only use this method within a worker thread.
-	//	If called from the main thread, it will freeze if the queue is empty.
+bool OrderQueue::WorkerPop(Order &outOrder) {
+  // Wait to be notified about an order in the queue.
+  std::unique_lock lock(mutex);
+  cv.wait(lock, [this] { return !orderQueue.empty() || shuttingDown; });
+  //	Only use this method within a worker thread.
+  //	If called from the main thread, it will freeze if the queue is empty.
 
-	if (orderQueue.empty()) {
-		// Only gets here if `shuttingDown == true`.
-		return false;
-	}
-	outOrder = orderQueue.front();
-	orderQueue.pop();
-	return true;
+  if (orderQueue.empty()) {
+    // Only gets here if `shuttingDown == true`.
+    return false;
+  }
+  outOrder = orderQueue.front();
+  orderQueue.pop();
+  return true;
 }
 
 size_t OrderQueue::Size() const {
-	std::lock_guard<std::mutex> lock(mutex);
-	return orderQueue.size();
+  std::lock_guard lock(mutex);
+  return orderQueue.size();
 }
 
 bool OrderQueue::IsEmpty() const {
-	std::lock_guard<std::mutex> lock(mutex);
-	return orderQueue.empty();
+  std::lock_guard lock(mutex);
+  return orderQueue.empty();
+}
+
+void OrderQueue::WaitUntilEmpty() {
+  std::unique_lock lock(mutex);
+  cv.wait(lock, [this]() { return orderQueue.empty(); });
 }
 
 void OrderQueue::Shutdown() {
-// Wakes all `WorkerPop` threads to shutdown.
-	{
-		std::lock_guard<std::mutex> lock(mutex);
-		shuttingDown = true;
-	}
-	cv.notify_all();
+  // Wakes all `WorkerPop` threads to shutdown.
+  {
+    std::lock_guard lock(mutex);
+    shuttingDown = true;
+  }
+  cv.notify_all();
 }
-
--- PharmaServe/OrderQueue.h
+++ PharmaServe/OrderQueue.h
@@ -1,28 +1,28 @@
 #pragma once
-#include <queue>
-#include <mutex>
 #include <condition_variable>
+#include <mutex>
+#include <queue>
 
 #include "Order.h"
 
 class OrderQueue {
-    private: 
-        mutable std::mutex mutex;
-        std:: condition_variable cv;
-        std::queue<Order> orderQueue;
-        bool shuttingDown = false;
+private:
+  mutable std::mutex mutex;
+  std::condition_variable cv;
+  std::queue<Order> orderQueue;
+  bool shuttingDown = false;
 
+public:
+  void Push(const Order &order);
 
-    public:
-        void Push(const Order& order);
-    
-        bool WorkerPop(Order& outOrder);
+  bool WorkerPop(Order &outOrder);
 
-        bool Pop(Order& outOrder);
-        
-		size_t Size() const;
-		bool IsEmpty() const;
+  bool Pop(Order &outOrder);
 
-        void Shutdown();
+  size_t Size() const;
+  bool IsEmpty() const;
 
+  void Shutdown();
+
+  void WaitUntilEmpty();
 };
