ca9c3b00977732a2519a7c5f80beb3992002b010
Owen Meyers <omeyers31@gmail.com>
2026-07-16 22:28:37 -0500

    Add WaitUntilEmpty test to OrderQueueTests.

--- tests/OrderQueueTests.cpp
+++ tests/OrderQueueTests.cpp
@@ -1,57 +1,79 @@
+#include <chrono>
 #include <gtest/gtest.h>
 #include <thread>
-#include <chrono>
 
 #include "../PharmaServe/OrderQueue.h"
 
 TEST(OrderQueueTest, StartsEmpty) {
-	OrderQueue queue;
-	EXPECT_TRUE(queue.IsEmpty());
-	EXPECT_EQ(queue.Size(), 0u);
+  OrderQueue queue;
+  EXPECT_TRUE(queue.IsEmpty());
+  EXPECT_EQ(queue.Size(), 0u);
 }
 
 TEST(OrderQueueTest, PushIncreasesSize) {
-	OrderQueue queue;
-	queue.Push({1, "Amoxicillin", 30});
-	EXPECT_FALSE(queue.IsEmpty());
-	EXPECT_EQ(queue.Size(), 1u);
+  OrderQueue queue;
+  queue.Push({1, "Amoxicillin", 30});
+  EXPECT_FALSE(queue.IsEmpty());
+  EXPECT_EQ(queue.Size(), 1u);
 }
 
 TEST(OrderQueueTest, PopReturnsOrdersInFifoOrder) {
-	OrderQueue queue;
-	queue.Push({1, "Amoxicillin", 30});
-	queue.Push({2, "Tylenol", 90});
+  OrderQueue queue;
+  queue.Push({1, "Amoxicillin", 30});
+  queue.Push({2, "Tylenol", 90});
 
-	Order first;
-	ASSERT_TRUE(queue.WorkerPop(first));
-	EXPECT_EQ(first.orderId, 1);
+  Order first;
+  ASSERT_TRUE(queue.WorkerPop(first));
+  EXPECT_EQ(first.orderId, 1);
 
-	Order second;
-	ASSERT_TRUE(queue.WorkerPop(second));
-	EXPECT_EQ(second.orderId, 2);
+  Order second;
+  ASSERT_TRUE(queue.WorkerPop(second));
+  EXPECT_EQ(second.orderId, 2);
 }
 
 TEST(OrderQueueTest, PopFailsOnEmptyQueue) {
-	OrderQueue queue;
-	Order order;
-	EXPECT_FALSE(queue.Pop(order));
+  OrderQueue queue;
+  Order order;
+  EXPECT_FALSE(queue.Pop(order));
 }
 
 TEST(OrderQueueTest, ShutdownUnblocksWaitingPop) {
-	OrderQueue queue;
-	bool popResult = false;
+  OrderQueue queue;
+  bool popResult = false;
 
-	std::thread worker([&queue, &popResult]() {
-		Order order;
-		queue.WorkerPop(order);
-		popResult = true;
-	});
+  std::thread worker([&queue, &popResult]() {
+    Order order;
+    queue.WorkerPop(order);
+    popResult = true;
+  });
 
-	std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
-	EXPECT_FALSE(popResult);
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  EXPECT_FALSE(popResult);
 
-	queue.Shutdown();
-	worker.join();
+  queue.Shutdown();
+  worker.join();
 
-	EXPECT_TRUE(popResult);
+  EXPECT_TRUE(popResult);
 }
+
+TEST(OrderQueueTest, WaitUntilEmptyBlocksUntilQueueDrained) {
+  OrderQueue queue;
+  queue.Push({1, "Aspirin", 100});
+  queue.Push({2, "Ibuprofen", 50});
+
+  std::atomic<bool> waitFinished(false);
+  std::thread waiting_thread([&queue, &waitFinished]() {
+    queue.WaitUntilEmpty();
+    waitFinished = true;
+  });
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  EXPECT_FALSE(waitFinished);
+
+  Order order;
+  ASSERT_TRUE(queue.Pop(order));
+  ASSERT_TRUE(queue.Pop(order));
+
+  waiting_thread.join();
+  EXPECT_TRUE(waitFinished);
+}
+
