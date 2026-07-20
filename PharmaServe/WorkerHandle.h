7249608daa52062b939a1df22d03856976dc203f
Owen Meyers <omeyers31@gmail.com>
2026-07-19 20:23:08 -0500

    [feat/WorkerManager] Define WorkerHandle struct.

--- PharmaServe/WorkerHandle.h
+++ PharmaServe/WorkerHandle.h
@@ -3,3 +3,15 @@
 // 1 thread,
 // 1 atomic<bool> to signal shutdown,
 // 1 WorkerStatus to hold info,
+
+#pragma once
+#include <atomic>
+#include <thread>
+
+#include "WorkerStatus.h"
+
+struct WorkerHandle {
+  std::thread worker;
+  std::atomic<bool> active;
+  WorkerStatus status;
+};
